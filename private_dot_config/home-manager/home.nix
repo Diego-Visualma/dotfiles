90716a0ecbf43c6f096ae373c4ff9eb3e79a5dd1
Diego-Visualma <mora@visualma.com>
2023-06-05 01:42:57 -0600

    move tmux and alacritty config to home-manager, and create wrapper for running nix alacritty with gpu

--- private_dot_config/home-manager/home.nix
+++ private_dot_config/home-manager/home.nix
@@ -24,21 +24,21 @@
    
     pkgs.starship
     pkgs.htop
+    pkgs.gotop
+    pkgs.ncdu
+    pkgs.duf
     pkgs.zoxide
     pkgs.fzf
-    pkgs.exa
-    pkgs.duf
-    pkgs.ripgrep
-    pkgs.ncdu
     pkgs.fd
-    pkgs.gotop
-    pkgs.neovim
-    pkgs.alacritty
+    pkgs.exa
+    pkgs.bat
+    pkgs.ripgrep
     pkgs.graphviz
-    pkgs.helix
     pkgs.ffmpeg_6-full
     pkgs.mpv
     pkgs.tealdeer
+    pkgs.djv
+    pkgs.ranger
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -52,6 +52,10 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
+    (pkgs.writeShellScriptBin "alacritty_gl" ''
+	__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia nixGL alacritty
+    '')
+
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -80,7 +84,7 @@
   #
   # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
-    EDITOR = "nvim";
+    # EDITOR = "nvim";
   };
 
   # Let Home Manager install and manage itself.
@@ -98,6 +102,99 @@
       { name = "sponge"; src = pkgs.fishPlugins.sponge.src; } 
     ];
 
+  };
+
+  programs.tmux = {
+    enable = true;
+    terminal = "screen-256color";
+    shell = "/home/diego/.nix-profile/bin/fish";
+    prefix = "C-a";
+    mouse = true;
+    extraConfig = "
+      # split panes using | and -
+      bind | split-window -h
+      bind - split-window -v
+      unbind '\"'
+      unbind %
+
+      # switch panes using Alt-arrow without prefix
+      bind -n M-Left select-pane -L
+      bind -n M-Right select-pane -R
+      bind -n M-Up select-pane -U
+      bind -n M-Down select-pane -D
+
+      # don't rename windows automatically
+      set-option -g allow-rename off
+
+      bind-key Bspace run-shell -b 'ranger'
+    ";
+    plugins = with pkgs; [
+    	tmuxPlugins.sensible 
+	tmuxPlugins.nord 
+	tmuxPlugins.prefix-highlight 
+	tmuxPlugins.tmux-fzf 
+    ];
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window.padding = {
+        x = 5;
+	y = 5;
+      };
+      window.opacity = 0.95;
+      font.normal = {
+        family = "FiraCode Nerd Font";
+	style = "Regular";
+      };
+      font.bold = {
+        family = "FiraCode Nerd Font";
+	style = "SemiBold";
+      };
+      font.size = 12.0;
+      shell.program = "/home/diego/.nix-profile/bin/fish";
+      shell.args = ["-c tmux"];
+      # Nord theme
+      colors = {
+        primary.background = "0x2E3440";
+	primary.foreground = "0xD8DEE9";
+	normal.black = "0x3B4252";
+	normal.red = "0xBF616A";
+	normal.green = "0xA3BE8C";
+	normal.yellow = "0xEBCB8B";
+	normal.blue = "0x81A1C1";
+	normal.magenta = "0xB48EAD";
+	normal.cyan = "0x88C0D0";
+	normal.white = "0xE5E9F0";
+	bright.black = "0x4C566A";
+	bright.red = "0xBF616A";
+	bright.green = "0xA3BE8C";
+	bright.yellow = "0xEBCB8B";
+	bright.blue = "0x81A1C1";
+	bright.magenta = "0xB48EAD";
+	bright.cyan = "0x8FBCBB";
+	bright.white = "0xECEFF4";
+      };
+      # For importing other themes:
+      #import = [ "{path-to-theme}.yml" ];
+    };
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  programs.helix = {
+    enable = true;
+    settings = {
+      theme = "ayu_mirage";
+      editor = {
+        line-number = "absolute";
+	mouse = true;
+      };
+    };
   };
 
   programs.emacs = {
--- run_before_install_nix_homemanager.sh
+++ run_before_install_nix_homemanager.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
 
 # Install nix if not installed.
-if [ ! command -v nix &> /dev/null ]
-then
+if command -v nix > /dev/null 2>&1 ; then
+	echo "Nix was already installed"
+else
 	echo "Nix is not installed. Starting installation"
 	curl -L https://nixos.org/nix/install | sh -s -- --daemon
 	echo "Nix has been installed"
-else
-	echo "Nix was already installed"
 fi
 
 # Install nixGL if not installed.
-if [ ! command -v nixGL &> /dev/null ]
-then
+if command -v nixGL > /dev/null 2>&1 ; then
+	echo "nixGL was already installed."
+else
 	echo "nixGL is not installed."
 	nix-channel --add https://github.com/guibou/nixGL/archive/main.tar.gz nixgl
 	nix-channel --update
 	nix-env -iA nixgl.auto.nixGLDefault
 	echo "nixGL has been installed"
-else
-	echo "nixGL was already installed."
 fi
 
 # Install home-manager if not installed.
-if [ ! command -v home-manager &> /dev/null ]
-then
+if command -v home-manager > /dev/null 2>&1 ; then
+	echo "Home-manager was already installed"
+else
 	echo "Home-manager is not installed."
 	nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
 	nix-channel --update
 	nix-shell '<home-manager>' -A install
 	echo "Home-manager has been installed."
-else
-	echo "Home-manager was already installed"
 fi
